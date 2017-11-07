f92e55964924ce79ff2571be6461fc1a9857e830
Christophe Grand <christophe@cgrand.net>
2017-11-07 15:52:15 +0100

    Mark clojure and clojurescript as provided

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject net.cgrand/macrovich "0.2.0"
+(defproject net.cgrand/macrovich "0.2.1-SNAPSHOT"
   :description "A set of three macros to ease writing `*.cljc` supporting Clojure, Clojurescript and self-hosted Clojurescript."
   :url "https://github.com/cgrand/macrovich"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.293"]])
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.293" :scope "provided"]])
