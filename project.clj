c39f9fe6e37f6fec352c621d3510ac02015fca42
Christophe Grand <christophe@cgrand.net>
2023-07-12 16:40:16 +0200

    bump to 0.2.2

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject net.cgrand/macrovich "0.2.1"
-  :description "A set of three macros to ease writing `*.cljc` supporting Clojure, Clojurescript and self-hosted Clojurescript."
+(defproject net.cgrand/macrovich "0.2.2"
+  :description "A set of three macros to ease writing `*.cljc` supporting Clojure, Clojurescript, self-hosted Clojurescript and ClojureDart."
   :url "https://github.com/cgrand/macrovich"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
